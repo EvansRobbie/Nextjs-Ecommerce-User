9ea0856e72487415fae77ebb01817947e28a9e35
EvansRobbie <evansitoden94@gmail.com>
2023-09-21 00:28:26 +0300

    try and fix api deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,39 +4,39 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
 //@ts-ignore
-// let base_url: string;
-// if (process.env.NODE_ENV === 'development'){
-//   base_url= pro
-// }else{
-//   base_url = 'next-js-ecommerce-admin-gilt.vercel.app'
-// }
+let base_url;
+console.log(base_url)
+if (process.env.NODE_ENV === 'development'){
+  base_url= 'http://localhost:3001'
+}else{
+  base_url = ''
+}
 const getProduct = async () =>{
-  const response = await fetch(`/api/hero/`, {cache:'no-store'})
-  if(!response.ok){
-    return notFound()
-  }
-  return response.json()
+  const response = await fetch(`http://localhost:3001/api/hero`, {cache:'no-store'})
+  // if(!response.ok){
+  //   return notFound()
+  // }
+  // return response.json()
   // console.log(response)
 }
 
 const getRecentProduct = async () =>{
-  const response = await fetch(`/api/recent_products`, { next: { revalidate: 3600 } })
-  if(!response.ok){
-    return notFound()
-  }
-  return response.json()
+  // const response = await fetch('http://localhost:3001/api/recent_products', { next: { revalidate: 3600 } })
+  // if(!response.ok){
+  //   return notFound()
+  // }
+  // return response.json()
   // console.log(response)
 }
 export default async function Home() {
-  const data = await getProduct()
+  // const data = await getProduct()
 
-  const recentProducts  = await getRecentProduct()
+  // const recentProducts  = await getRecentProduct()
   // console.log(recentProducts)
   // console.log(data)
   return (
     <main className="">
-      <Featured data= {data}/>
-      <NewProducts recent = {recentProducts}/>
+     
     </main>
   )
 }
